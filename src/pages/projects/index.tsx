61d6c7f857e383c1af4f0a8608519d46615ca8f4
RoshenH <roshen.20210497@gmail.com>
2023-10-30 13:53:49 +0530

    package page update

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,36 @@
-import {Button, Card, CardActions, CardContent, Stack, Typography} from '@mui/material';
 import React, {useState} from 'react';
 import MiniDrawer from "../../layouts/side-bar";
 import "./styles/styles.css"
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';import OutlinedInput from '@mui/material/OutlinedInput';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+];
 
 function ShowFunction(){
     const [visible,SetChecked]=useState(false);
@@ -9,8 +38,43 @@ function ShowFunction(){
     const checkBox = () =>{
         SetChecked(!visible)
     }
+    const [age, setAge] = React.useState('');
 
-    const [serviceList,setServiceList] = useState([{service: ""},{service: ""}])
+
+
+
+
+
+
+    const [visiblity, setVisible] = useState(false);
+
+    const [a, setVisi] = useState(false);
+
+    const [personName, setPersonName] = React.useState<string[]>([]);
+
+    const [personNames, setPersonNames] = React.useState<string[]>([]);
+
+
+    const handleChanges = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    const handleChanges2 = (event: SelectChangeEvent<typeof personNames>) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonNames(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
 
 
 
@@ -65,7 +129,7 @@ function ShowFunction(){
                         <div className="input-data">
                             <input type="text" required/>
                             <div className="underline"></div>
-                            <label htmlFor="">Project Lead's Email</label>
+                            <label htmlFor="">Client's Lead's Email</label>
                         </div>
                     </div>
                     <div className="form-row">
@@ -160,72 +224,149 @@ function ShowFunction(){
 
                     <h2>Inova Details</h2>
 
+
                     <div className="form-row">
                         <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Project Lead's Name</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Designation</label>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-multiple-checkbox-label">project Leads</InputLabel>
+                                <Select
+                                    labelId="demo-multiple-checkbox-label"
+                                    id="demo-multiple-checkbox"
+                                    multiple
+                                    value={personName}
+                                    onChange={handleChanges}
+                                    input={<OutlinedInput label="Tag" />}
+                                    renderValue={(selected) => selected.join(', ')}
+                                    MenuProps={MenuProps}
+                                >
+                                    {names.map((name) => (
+                                        <MenuItem key={name} value={name}>
+                                            <Checkbox checked={personName.indexOf(name) > -1} />
+                                            <ListItemText primary={name} />
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
                         </div>
                     </div>
-                    <div className="form-row">
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Project Lead's Contact Number</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Project Lead's Email</label>
-                        </div>
+
+                    <div className={"radio"}>
+                        <label className="radio label"> Add New Project Lead: </label>
+
+                                Yes <input type="radio" className="mx-2" name="isyes" value="1" onClick={ ()=>setVisible(true)} />
+
+                                No <input type="radio" className="mx-2 mt-1" name="isyes" value="0" onClick={ ()=>setVisible(false)} />
 
                     </div>
-                    <div className="form-row">
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Project Lead's Private Email</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Specialized Field</label>
-                        </div>
 
-                    </div>
+
+                    { visiblity &&
+                        <div>
+                            <div className="form-row">
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Project Lead's Name</label>
+                                </div>
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Project Lead's Email</label>
+                                </div>
+
+                            </div>
+                            <div className="form-row">
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Project Lead's Contact Number</label>
+                                </div>
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Project Lead's Private Email</label>
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Specified Field</label>
+                                </div>
+                            </div>
+
+                        </div>
+                    }
 
                     <h2>Project Estimators</h2>
-
                     <div className="form-row">
                         <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Name</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Designation</label>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-multiple-checkbox-label">Project Estimators</InputLabel>
+                                <Select
+                                    labelId="demo-multiple-checkbox-label"
+                                    id="demo-multiple-checkbox"
+                                    multiple
+                                    value={personNames}
+                                    onChange={handleChanges2}
+                                    input={<OutlinedInput label="Tag" />}
+                                    renderValue={(selected) => selected.join(', ')}
+                                    MenuProps={MenuProps}
+                                >
+                                    {names.map((name) => (
+                                        <MenuItem key={name} value={name}>
+                                            <Checkbox checked={personNames.indexOf(name) > -1} />
+                                            <ListItemText primary={name} />
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
                         </div>
                     </div>
-                    <div className="form-row">
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Name</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required/>
-                            <div className="underline"></div>
-                            <label htmlFor="">Designation</label>
-                        </div>
+                    <div className={"radio"}>
+                        <label className="col-sm-3 col-form-label"><span> Add New Project Estimators: </span></label>
+                        Yes <input type="radio" className="mx-2" name="isyes" value="1" onClick={ ()=>setVisi(true)} />
+
+                        No <input type="radio" className="mx-2 mt-1" name="isyes" value="0" onClick={ ()=>setVisi(false)} />
 
                     </div>
+
+                    {a &&
+                        <div>
+                            <div className="form-row">
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Effort Estimator's Name</label>
+                                </div>
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Effort Estimator's Email</label>
+                                </div>
+
+                            </div>
+                            <div className="form-row">
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Effort Estimator's Contact Number</label>
+                                </div>
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Effort Estimator's Private Email</label>
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="input-data">
+                                    <input type="text" required/>
+                                    <div className="underline"></div>
+                                    <label htmlFor="">Specified Field</label>
+                                </div>
+                            </div>
+
+                        </div>}
 
 
                     <h2>Other Details</h2>
